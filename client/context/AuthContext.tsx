4df89911fa2eb30094677609ffb62f8c274f27b6
Jeezman <tobiadeyemi91@gmail.com>
2022-05-24 06:46:49 +0100

    Update frontend to receive socket data

--- client/context/AuthContext.tsx
+++ client/context/AuthContext.tsx
@@ -1,5 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { loginWithLN } from '../api';
+import { io } from 'socket.io-client';
+import { useRouter } from 'next/router';
+
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || '';
+const socket = io(SOCKET_URL, {
+  transports: ['websocket'],
+});
 
 interface LNData {
   encoded: string;
@@ -25,6 +32,19 @@ export const AuthContext = React.createContext<IAuthContext>(defaultState);
 
 export const AuthContextProvider = ({ children }: Props) => {
   const [lnData, setLnData] = useState(defaultState.lnData);
+
+   const router = useRouter();
+
+  useEffect(() => {
+    const getEventsSocket = () => {
+      socket.on('auth', (data: any) => {
+        if (data.key) {
+          router.push('/dashboard/');
+        }
+      });
+    };
+    getEventsSocket()
+  }, [router]);
 
   const handleLoginWithLN = async () => {
     let response = await loginWithLN();
